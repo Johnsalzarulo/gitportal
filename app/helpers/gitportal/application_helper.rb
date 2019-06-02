49f8baa0659a3f0103197b8ecc6a7321e155a84c
Manuel Ortega <manuel@lasindias.coop>
2019-06-02 02:45:54 -0700

    Makes main app routes helpers working when rendering engine's views. From http://www.candland.net/2012/04/17/rails-routes-used-in-an-isolated-engine/

--- app/helpers/gitportal/application_helper.rb
+++ app/helpers/gitportal/application_helper.rb
@@ -1,4 +1,9 @@
 module Gitportal::ApplicationHelper
+
+  def method_missing method, *args, &block
+    method.to_s.end_with?('_path', '_url') and main_app.respond_to?(method) ? main_app.send(method, *args) : super
+  end
+
   def markdown(text)
     options = {
       filter_html:     false,
