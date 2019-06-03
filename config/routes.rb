c9902a5dbbbfbac2ab9b7e30e63297d17e431309
Manuel Ortega <manuel@lasindias.coop>
2019-06-03 02:40:00 -0700

    Implemented Pull requests/changes feauture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Gitportal::Engine.routes.draw do
   resources :issues, only: %i[index show]
-  resources :milestones, only: %i[index show]
   resources :pull_requests, only: %i[index show]
+  resources :milestones, only: %i[index show]
 end
